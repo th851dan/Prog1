b1089b33fc817918a226ccdcb76f4a4df6221b41
Julian Riegraf <julianriegraf@web.de>
2020-04-08 16:42:54 +0200

    update

--- export.sh
+++ export.sh
@@ -1,2 +1,3 @@
 pandoc -f markdown-raw_tex Anleitung_Installation_Werkzeuge.md -o Anleitung_Installation_Werkzeuge.html --from markdown
+pandoc -f markdown-raw_tex Anleitung_Installation_Werkzeuge.md -o Anleitung_Installation_Werkzeuge.docx --from markdown
 pandoc -f markdown-raw_tex -V geometry:margin=2cm Anleitung_Installation_Werkzeuge.md -o Anleitung_Installation_Werkzeuge.pdf --from markdown
